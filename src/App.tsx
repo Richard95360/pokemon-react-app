43cc318c5bf6ae985b8913e1d1a25dbe419a69a1
Richard95360 <richard.haddad1@free.fr>
2021-06-02 09:43:32 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React,{FunctionComponent} from 'react'
 import {BrowserRouter as Router, Route,Switch, Link  } from 'react-router-dom';
+import Login from './pages/login';
 import PageNotFound from './pages/page-not-found';
 import PokemonAdd from './pages/pokemon-add';
 import PokemonsDetail from './pages/pokemon-detail';
 import PokemonEdit from './pages/pokemon-edit';
 import PokemonList from './pages/pokemon-list';
-
+import PrivateRoute from './PrivateRoute';
 
 const App: FunctionComponent = () => {
 
@@ -19,11 +20,12 @@ const App: FunctionComponent = () => {
           </div>
         </nav>
         <Switch>
-          <Route exact path="/" component={PokemonList} />
-          <Route exact path="/pokemons" component={PokemonList} />
-          <Route exact path="/pokemon/add" component={PokemonAdd} />
-          <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
-          <Route  path="/pokemons/:id" component={PokemonsDetail} />
+          <PrivateRoute exact path="/" component={PokemonList} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/pokemons" component={PokemonList} />
+          <PrivateRoute exact path="/pokemon/add" component={PokemonAdd} />
+          <PrivateRoute exact path="/pokemons/edit/:id" component={PokemonEdit} />
+          <PrivateRoute  path="/pokemons/:id" component={PokemonsDetail} />
         
           <Route   component={PageNotFound} />
         </Switch>
